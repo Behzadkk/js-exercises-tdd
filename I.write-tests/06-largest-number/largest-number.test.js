4d2c6e37cae3554f7913d7543caa486ccee42c62
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2019-01-19 13:16:55 +0000

    unchanged array

--- I.write-tests/06-largest-number/largest-number.test.js
+++ I.write-tests/06-largest-number/largest-number.test.js
@@ -3,23 +3,16 @@ test("get the largest number", function() {
   //Arrange
   let expectedResult = 88;
   let input = [3, 21, 88, 4, 36];
+  let unchanged = [3, 21, 88, 4, 36];
   let result;
   //Act
   result = getLargestNumber(input);
   //Assert
   expect(result).toEqual(expectedResult);
+  expect(input).toEqual(unchanged);
 });
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
 
 // also test that the original array hasn't changed
-test("original array has not changed", function() {
-  let expectedResult = [3, 21, 88, 4, 36];
-  let input = [3, 21, 88, 4, 36];
-  let sideEffects;
-  //Act
-  sideEffects = getLargestNumber(input);
-  //Assert
-  expect(input).toEqual(expectedResult);
-});
