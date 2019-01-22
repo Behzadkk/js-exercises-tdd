3518a6daae97c39ec7a93e3ac56e701ff9dd9bf7
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2019-01-22 10:36:28 +0000

    paint shop hard code answer

--- II.pass-tests/10-paint-ford/paint-cars.js
+++ II.pass-tests/10-paint-ford/paint-cars.js
@@ -1,10 +1,25 @@
 module.exports = paintShop;
-function paintShop(cars, colour) {
-  const repaintedCars = cars.map(car => car);
-  repaintedCars.map(paintedCar => {
-    if (paintedCar.make === "Ford") {
-      paintedCar.colour = colour;
+// function paintShop(cars, colour) {
+//   const repaintedCars = cars.map(car => car);
+//   repaintedCars.map(paintedCar => {
+//     if (paintedCar.make === "Ford") {
+//       paintedCar.colour = colour;
+//     }
+//   });
+//   return repaintedCars;
+// }
+
+function paintShop(cars, newColor) {
+  let newCars = cars.map(car => {
+    if (car.make == "Ford") {
+      return {
+        make: "Ford",
+        model: "Fiesta",
+        colour: newColor
+      };
+    } else {
+      return car;
     }
   });
-  return repaintedCars;
+  return newCars;
 }
